19da7840c596c961fff0c2a234669870c5ee47f0
niranjan <niranjan.hulamudde123@gmail.com>
2026-09-16 15:25:54 +0530

    Modified main.tf file

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ resource "aws_instance" "app_server" {
 	ami 	= data.aws_ami.ubuntu.id
 	instance_type	= var.instance_type
 
+	key_name = "awskey"	# Tells aws to use these key for ssh authentication
+
 	tags = 	{
 	Name	= var.server_name
 }
